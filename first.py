c1d42551a0275760bdbfa6716c1a00f62020f891
Junaid Mahmud <junaidmahmud@iut-dhaka.edu>
2022-03-17 20:34:49 +0600

    first.py

--- first.py
+++ first.py
@@ -1,35 +1,27 @@
-## Second 
+# Second
 
 flag = 1
 
 while flag == 1:
-    
-    hex_nums = Null # Initialize this variable for hex numbers
-                    # greater than 0
-                    
-    
-    number = # Take input for the number that you want to convert
-    num_system = # Take input for the number system
+
+    hex_nums = Null  # Initialize this variable for hex numbers
+    # greater than 0
+
+    number =  # Take input for the number that you want to convert
+    num_system =  # Take input for the number system
 
     updater = number
     result = ""
 
     while updater != 0:
-    
-        rem = # Store the remainder of updater
+
+        rem =  # Store the remainder of updater
         result = str(rem) + str(result)
         updater =   # saving the integer part of the quotient
-                    ## Use anything except int(updater/2)
-    
+        # Use anything except int(updater/2)
+
     print(f'Result: {result}')
-    
+
     flag = int(input("\n\n\n\n\npress:\n\t1 if again\n\t0 if close\n"))
 
 print("\n\nThank you!")
-
-    
-    
-
-
- 
-
