ee511911dd2528e26a00075437cae72c057c8b14
Junaid Mahmud <junaidmahmud@iut-dhaka.edu>
2022-04-17 21:24:15 +0600

    sum.py added

---
+++ Basics/sum.py
@@ -0,0 +1,10 @@
+
+'''
+    => Implement the sum() function on a list
+    without using the sum() function
+    
+    => I will give you a list, find the sum 
+    of the elements of the list, without using
+    sum() function.
+    [hint: You have to use loop]
+'''
