98bda47481f72b91482c72ff3dda3f74150427f6
Junaid Mahmud <junaidmahmud@iut-dhaka.edu>
2022-04-17 21:30:05 +0600

    sum.py updated

---
+++ Basics/sum.py
@@ -0,0 +1,15 @@
+
+'''
+
+    => Take numbers input and store them in a list.
+    Then convert the numbers into integers.
+    The elements are: 15 20 10 30 5
+    
+    => Implement the sum() function on a list
+    without using the sum() function
+    
+    => I will give you a list, find the sum 
+    of the elements of the list, without using
+    sum() function.
+    [hint: You have to use loop]
+'''
