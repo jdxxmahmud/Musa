b1051b9dd2370fefef97d6b01e069131a90cc064
Junaid Mahmud <junaidmahmud@iut-dhaka.edu>
2022-04-17 21:20:08 +0600

    Class 7

---
+++ Basics/list.py
@@ -0,0 +1,19 @@
+from time import sleep
+
+
+my_list = input("Input the numbers: ").split(" ")
+len_of_my_list = len(my_list)
+
+
+# List: 65 35 74 90 81 72 35 18
+sum = 0
+for i in my_list:  # You can not update my_list using i
+    print(i)
+
+print("Using another loop \n\n\n")
+sleep(5)
+
+# You can change your list using this, basically
+for i in range(0, len_of_my_list):  # You can update my_list using i
+    my_list[i] = int(my_list[i])
+    print(my_list[i])
---
+++ Basics/range.py
@@ -0,0 +1,4 @@
+my_list = [1, 2, 3, 4]
+for i in len(my_list):  # You can put a list or a range here
+    print(i)  # If you put len(my_list), that will be
+    # an integer, which will show error
