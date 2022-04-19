0e39e8ebdf3e208b7005d0379bb9ddcd84750e6e
MusaSudad <musasudad271@gmail.com>
2022-04-19 11:17:25 +0600

    Homework Done

--- Basics/sum.py
+++ Basics/sum.py
@@ -13,11 +13,18 @@
     sum() function.
     [hint: You have to use loop]
 '''
-list1 = input("Enter you number here with \"_\" as a separator")
-list1 = list1.split("_")
+list1 = input("Enter you number: ")
+list1 = list1.split(" ")
 len_of_list = len(list1)
 
 for i in range(len_of_list) :
     list1[i] = int(list1[i])
 
 print(list1)
+
+Total = 0 
+
+for i in list1 :
+    Total = Total + i  
+
+print(f"Total {Total}")
