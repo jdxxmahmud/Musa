040cc2725be42dce6a3755a072b78981cacb9c22
Junaid Mahmud <junaidmahmud@iut-dhaka.edu>
2022-04-20 20:30:23 +0600

    codewars

---
+++ Codewars/to_sqr_rt_or_not.py
@@ -0,0 +1,12 @@
+# Problem Link: https://www.codewars.com/kata/57f6ad55cca6e045d2000627
+# Problem Name: To square(root) or not to square(root)
+
+
+def square_or_square_root(arr):
+    pass
+
+
+arr = [4, 3, 9, 7, 2, 1]
+ans = square_or_square_root(arr)
+result = [2, 9, 3, 49, 4, 1]
+print("Accepted" if result == ans else "Not Accepted")
