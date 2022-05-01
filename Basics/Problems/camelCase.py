69ffdb6a9cb44bebc980e631300ee27c0b2c5f5e
jdxxmahmud <junaidmahmud@iut-dhaka.edu>
2022-05-01 20:43:16 +0600

    camelCase File Added

---
+++ Basics/Problems/camelCase.py
@@ -0,0 +1,20 @@
+### Convert a string into camelcase string
+
+### Input: "My name is musa"
+### Output: "My NaMe Is MuSa"
+
+
+import string
+
+
+def camelCase(str: string):
+    pass
+
+
+def main():
+    camelCase("I am in Bangladesh")
+    camelCase("You and me are doing codes")
+
+if __name__ == "main":
+    main()
+
--- Basics/strings.py
+++ Basics/strings.py
@@ -1,3 +1,4 @@
 
 import string
 print("a".upper())
+
