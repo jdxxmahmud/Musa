31d195b9d72079ec76b93a141b1df4f385074c43
MusaSudad <musasudad271@gmail.com>
2022-05-01 20:19:07 +0600

    Merge branch 'main' of https://github.com/jdxxmahmud/Musa

--- Basics/list.py
+++ Basics/list.py
@@ -10,11 +10,16 @@ for i in my_list:  # You can not update my_list using i
     print(i)
 
 print("Using another loop \n\n\n")
-sleep(5)
+sleep(3.5)
 
 # You can change your list using this, basically
 for i in range(0, len_of_my_list):  # You can update my_list using i
     my_list[i] = int(my_list[i])
     print(my_list[i])
 
+    
 
+"""
+    my_list[i] = int(my_list[i])
+    print(my_list[i])
+"""
