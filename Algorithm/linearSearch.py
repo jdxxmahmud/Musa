4d309332bec4b0fa6dad98a12dc6b6e8f677cc7e
Junaid Mahmud <junaidmahmud@iut-dhaka.edu>
2022-05-11 19:58:06 +0600

    linearSearch introduced. First class of algorithm

---
+++ Algorithm/linearSearch.py
@@ -0,0 +1,33 @@
+'''
+It takes a l, and a key. 
+Returns the position of the key in the list,
+    * if the key is present in the list
+    * else return -1 
+'''
+
+
+def linearSearch(my_list: int, key: int):
+
+    len_list = len(my_list)
+
+    for i in range(len_list):
+        if my_list[i] == key:
+            return i
+
+    # The following will run if the key is not in the list
+    return -1
+
+
+def main():
+    # my_list = map(int, input("Enter list").split(" "))
+    # key = int(input("Enter key: "))
+
+    my_list = [10, 20, 30, 12, 6, 50, -2, 79]
+    key = 7
+
+    ans = linearSearch(my_list, key)
+
+    print(f"Correct, position is {ans}" if ans == 4 else "Wrong")
+
+
+main()
---
+++ Basics/sort.py
@@ -0,0 +1,7 @@
+## Sort: Ordering the elements based on their values.
+
+m_list = [20, 30, 1, 4, 25]
+
+sorted_list = sorted(m_list)
+
+print(sorted_list)
