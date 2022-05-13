ce4ee6180b479b66e16d43f389a4aae065067739
MusaSudad <musasudad271@gmail.com>
2022-05-13 08:29:01 +0600

    add subtract multiply divide mod and squared functions

---
+++ Homeworks/addition.py
@@ -0,0 +1,5 @@
+def add(a,b):
+    return print(a + b)
+
+
+    
---
+++ Homeworks/numoperations.py
@@ -0,0 +1,29 @@
+from re import sub
+
+
+def add (a, b):
+
+    return print(a + b)
+
+def subtract (a, b):
+    return print(a - b)
+
+def multiply (a , b):
+    return print(a * b)
+
+def divide (a, b):
+    return print(a / b)
+
+def squared (a: int):
+    return print(a **2)
+
+def mod  (a: int , b: int):
+    return print(a % b)
+
+
+add(18,2)
+subtract(18,2)
+multiply(18,2)
+divide(18,2)
+squared(18)
+mod(18,5)
