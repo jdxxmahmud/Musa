f197bd65df5cafcbcd674adce85ae9113ae91eb1
Junaid Mahmud <junaidmahmud@iut-dhaka.edu>
2022-06-12 21:18:12 +0600

    1328A

--- Codeforces/1328A.py
+++ Codeforces/1328A.py
@@ -2,16 +2,21 @@
 
 
 t = int(input())
-cases = []
 for _ in range(t):
     a, b = list(map(int, input().split(' ')))
     # print(a, b)
 
     # Solution code goes here
-    if not a % b:
-        print(0)
-    elif a < b:
-        print(b-a)
-    else:
-        print((b * ((a//b) + 1) - a))
+    # if not a % b:
+    #     print(0)
+    # elif a < b:
+    #     print(b-a)
+    # else:
+    #     print((b * ((a//b) + 1) - a))
+    
+    n = 0
+    while a % b != 0:
+        a += 1
+        n += 1
     ###
+    print(n)
