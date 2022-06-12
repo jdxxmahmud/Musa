ba53fbd8599f90263bf7cfb8a6622440f3cfb1ce
Junaid Mahmud <junaidmahmud@iut-dhaka.edu>
2022-06-12 20:38:11 +0600

    codeforces added

---
+++ Codeforces/1328A.py
@@ -0,0 +1,12 @@
+# Problem Link: https://codeforces.com/problemset/problem/1328/A
+
+
+t = int(input())
+cases = []
+for _ in range(t):
+    test_case = list(map(int, input().split(' ')))
+    print(test_case)
+
+    # Solution code goes here
+
+    ###
