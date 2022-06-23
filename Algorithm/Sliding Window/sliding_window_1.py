5b1c91bc56f515cd3d198e3bc88bccaa79111239
Junaid Mahmud <junaidmahmud@iut-dhaka.edu>
2022-06-23 21:04:43 +0600

    Sliding Window with o(n^2) complexity

---
+++ Algorithm/Sliding Window/sliding_window_1.py
@@ -0,0 +1,23 @@
+def max_sum(lst, k=3):
+
+    length = len(lst)
+    max_sum = -99999
+
+    for i in range(length - k):
+
+        current_sum = 0
+
+        for j in range(i, i + k):  # Getting the sum of the window
+            current_sum += lst[j]
+
+        if max_sum < current_sum:
+            indexes = []
+            max_sum = current_sum
+            for j in range(i, i + k):
+                indexes.append(j)
+
+    return max_sum, indexes
+
+
+lst = [1, 2, 3, 6, 7, 8, 1, 2, 9, 6, 2, 10, 5]
+print(max_sum(lst))
--- Codeforces/1328A.py
+++ Codeforces/1328A.py
@@ -6,17 +6,9 @@ for _ in range(t):
     a, b = list(map(int, input().split(' ')))
     # print(a, b)
 
-    # Solution code goes here
-    # if not a % b:
-    #     print(0)
-    # elif a < b:
-    #     print(b-a)
-    # else:
-    #     print((b * ((a//b) + 1) - a))
-    
-    n = 0
-    while a % b != 0:
-        a += 1
-        n += 1
-    ###
-    print(n)
+    if a % b == 0:
+        print(0)
+    elif a < b:
+        print(b-a)
+    else:
+        print(b * ((a//b) + 1) - a)
