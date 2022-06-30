01b342ee363445644207c3c6397d06917e65f5a2
Junaid Mahmud <junaidmahmud@iut-dhaka.edu>
2022-06-30 21:03:57 +0600

    Exam 1

---
+++ Exams/Exam 1/exam1_2.py
@@ -0,0 +1,41 @@
+
+'''
+Explain the following code
+
+for input
+5
+3
+ABA
+11
+DDBBCCCBBEZ
+7
+FFGZZZY
+1
+Z
+2
+AB
+
+'''
+
+
+def isSuspicious(tasks, n):
+    checker = [[0, 0] for _ in range(26)]
+
+    checker[ord(tasks[0]) - ord('A')][0] == 1
+    for i in range(1, n):
+        if checker[ord(tasks[i]) - ord('A')][1]:
+            return "NO"
+        checker[ord(tasks[i]) - ord('A')][0] = 1
+        if tasks[i - 1] != tasks[i]:
+            checker[ord(tasks[i - 1]) - ord('A')][1] = 1
+
+    return "YES"
+
+
+t = int(input())
+while t:
+    t -= 1
+    n = int(input())
+    tasks = input()
+
+    print(isSuspicious(tasks, n))
