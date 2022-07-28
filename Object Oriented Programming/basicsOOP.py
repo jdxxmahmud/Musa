ef342befb11f171adeb208fb30108e1dff3cd447
MusaSudad <musasudad271@gmail.com>
2022-07-28 20:36:52 +0600

    some files

---
+++ Object Oriented Programming/basicsOOP.py
@@ -0,0 +1,39 @@
+class identity:
+    name = 'Musa'#why is this called an object
+    age = 16
+    gender = "Male"
+    occupation = "Professional Racist"
+
+print(identity.name)
+print(identity.age)
+print(identity.occupation)
+print(identity.gender)
+
+class Person:
+    def __init__(self, name, age):
+         self.name = name
+         self.age = age
+
+#print(Person("Musa", 16))
+
+p1 = Person("musa", 16)
+p2 = Person("Sudad", 18)
+p3 = Person('ayan', 12)
+
+ask = input('')
+
+if ask == 'age':
+    print(p1.age)
+    #print(p1) - eta kono kichu dai na keno 
+else:
+    print(p1.name)
+
+class my_method:
+    def __init__(self, age):
+        self.age = age
+        def next_age(self):
+            return print(age+1)
+
+    
+x = my_method()
+
