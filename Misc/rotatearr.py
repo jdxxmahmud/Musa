c5933f69b056ee0c24083aa4ac830cf309dfe803
MusaSudad <musasudad271@gmail.com>
2022-12-09 17:24:33 +0600

    some changes

--- Misc/rotatearr.py
+++ Misc/rotatearr.py
@@ -1,10 +1,12 @@
+#problem statement - given a rotated sorted array, find the greatest number in O(logn)
+
 def rotateSearch(arr: list):
     hi = len(arr) - 1
     lo = 0 
     val = hi 
     
     while hi >= lo:
-        mid = (hi + lo) // 2 
+        mid = (hi + lo) // 2  
         if arr[mid] > arr[val]:
             val = mid 
         elif arr[mid] > arr[lo]:
@@ -14,22 +16,22 @@ def rotateSearch(arr: list):
 
     return val  
 
-arrOne = [1,2,3,4,5,6,7,8,9]
-arrTwo = [2,3,4,5,6,7,8,9,1]
-arrThree = [3,4,5,6,7,8,9,1,2]
-arrFour = [4,5,6,7,8,9,1,2,3]
-arrFive = [5,6,7,8,9,1,2,3,4]
+# arrOne = [1,2,3,4,5,6,7,8,9]
+# arrTwo = [2,3,4,5,6,7,8,9,1]
+# arrThree = [3,4,5,6,7,8,9,1,2]
+#arrFour = [4,5,6,7,8,9,1,2,3]
+# arrFive = [5,6,7,8,9,1,2,3,4]
 arrSix = [6,7,8,9,1,2,3,4,5]
 arrSeven = [7,8,9,1,2,3,4,5,6]
-arrEight = [8,9,1,2,3,4,5,6,7]
-arrNine = [9,1,2,3,4,5,6,7,8]
+# arrEight = [8,9,1,2,3,4,5,6,7]
+# arrNine = [9,1,2,3,4,5,6,7,8]
 
-print(rotateSearch(arrOne))
-print(rotateSearch(arrTwo))
-print(rotateSearch(arrThree))
-print(rotateSearch(arrFour))
-print(rotateSearch(arrFive))
+# print(rotateSearch(arrOne))
+# print(rotateSearch(arrTwo))
+# print(rotateSearch(arrThree))
+#print(rotateSearch(arrFour))
+# print(rotateSearch(arrFive))
 print(rotateSearch(arrSix))
 print(rotateSearch(arrSeven))
-print(rotateSearch(arrEight))
-print(rotateSearch(arrNine))
+# print(rotateSearch(arrEight))
+# print(rotateSearch(arrNine))
